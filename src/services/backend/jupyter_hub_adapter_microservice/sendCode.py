442c5b821e7968a7e93e5770410605b2248c736a
aziz <a.vundirov>
2025-06-07 17:10:09 +0300

    implement magic method

---
+++ src/services/backend/jupyter_hub_adapter_microservice/sendCode.py
@@ -0,0 +1,37 @@
+from http.client ***REMOVED***sponses
+
+from IPython.core.magic import Magics,magics_class, cell_magic
+
+***REMOVED***quests
+import os
+
+
+
+
+
+@magics_class
+class tellBackendMagic:
+
+    @cell_magic
+    def LLM_Validation(self, line, cell):
+
+        working_dir = line.strip()
+        self.path_is_valid(working_dir)
+        backend_URL = ...
+        try:
+            response = requests.post(backend_URL,json={"code":cell})
+            with open (f"{working_dir}/response.md") as f:
+                # md_response = response.json().get("validation")
+                md_response = "MD response from Aziz!!!"
+                f.write(md_response)
+        except requests.exceptions.RequestException as e:
+            print(f"[ERROR] {e}")
+
+
+    def path_is_valid(self, working_dir):
+        if (os.path.isdir(working_dir)):
+            raise Exception("Please, provide path to working directory")
+
+
+def load_ipython_extension(ipython):
+    ipython.registers_magic(tellBackendMagic)
