e65e9f7b6a589b68ad66a173bc325c6653655254
Your Name <you@example.com>
2025-06-11 21:16:02 +0000

    revert to hardcode version

--- src/services/backend/mediator_jupyter_hub_llm_microservice/service.py
+++ src/services/backend/mediator_jupyter_hub_llm_microservice/service.py
@@ -1,47 +1,30 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
+from pydantic import BaseModel
 
-import requests
-import os
 
-from dotenv import load_dotenv
-load_dotenv()
+class Code(BaseModel):
+	code: str
 
 app = FastAPI()
 
-@app.post("/getMdFeedback")
-async def get_md_feedback(
-	file: UploadFile = File(...),
-) -> FileResponse:
-	"""
-	Sends .ipynb for processing and returns .md result
-	"""
-	try:
-		content = await file.read()
-		content_str = content.decode()
+@app.get("/")
+async def root():
+	return {"Hello": "World!"}
 
-		files = {"file": (file.filename, content_str, "application/x-ipynb+json")}
 
-		FEEDBACK_URL = os.getenv("LLM_BASE_URL")
-
-		response = requests.post(
-			url=f"{FEEDBACK_URL}/api/v1/feedback",
-			files=files,
-			data=data,
-		)
+@app.post("/mdAnswer")
+async def mdAnswer(file: UploadFile = File(...)):
+	content = await file.read()
+	content_str = content.decode()
 	
-		file_path = "generated_file.md"
-		with open(file_path, "w") as f:
-			f.write(response.feedback)
+	file_path = "generated_file.md"
+	with open(file_path, "w") as f:
+		f.write("Hello World! Here is your code:")
+		f.write(content_str)
 
-		return FileResponse(
-			path=file_path,
-			media_type="text/markdown",
-			filename="generated_file.md",
-		)
-	except Exception as e:
-		raise HTTPException(
-			status_code=500,
-			detail=f"Failed to get .md feedback (mediator service error): {e}",
-		) from e
-
+	return FileResponse(
+		path=file_path,
+		media_type="text/markdown",
+		filename="generated_file.md"
+	)
