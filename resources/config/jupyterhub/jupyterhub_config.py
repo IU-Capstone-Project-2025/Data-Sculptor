f5ca1abeb1db36a0855b268b8bbf6c8ec803ce6b
Your Name <you@example.com>
2025-06-18 16:16:17 +0000

    integrate enviroment variables

--- resources/config/jupyterhub/jupyterhub_config.py
+++ resources/config/jupyterhub/jupyterhub_config.py
@@ -18,9 +18,14 @@ c.Spawner.debug = True
 import os
 
 LLM_VALIDATOR_URL = os.getenv('LLM_VALIDATOR_URL')
-if not LLM_VALIDATOR_URL:
+URL_STATIC_ANALYZER = os.getenv("URL_STATIC_ANALYZER")
+URL_LSP_SERVER = os.getenv("URL_LSP_SERVER")
+if not LLM_VALIDATOR_URL or not URL_LSP_SERVER or not URL_STATIC_ANALYZER:
     raise RuntimeError("LLM_VALIDATOR_URL env var is not specified")
 
 c.Spawner.environment = {
-    'LLM_VALIDATOR_URL': LLM_VALIDATOR_URL
+    'LLM_VALIDATOR_URL': LLM_VALIDATOR_URL,
+    'URL_STATIC_ANALYZER':URL_STATIC_ANALYZER,
+    'URL_LSP_SERVER':URL_LSP_SERVER
+
 }
