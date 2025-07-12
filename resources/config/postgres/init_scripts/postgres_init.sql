eb8f07af84a61b8ef3764588cc48fddfe91c3d9f
Marat M. <m.mestnikov@innopolis.university>
2025-07-12 13:50:33 +0300

    case uploader update

--- resources/config/postgres/init_scripts/postgres_init.sql
+++ resources/config/postgres/init_scripts/postgres_init.sql
@@ -36,7 +36,6 @@ create table if not exists profile_sections (
 create table if not exists cases (
     id uuid primary key default uuid_generate_v4(),
     name text not null,
-    profile_url text not null,
-    docker_image_url text not null,
+    bucket_url text not null,
     created_at timestamptz default now()
 );
